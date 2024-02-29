1aaaea2f2a7a3e7450f8ac3980d2d0c6a3a9bde1
Ferenc Visztra <ferenc.visztra@guidedknowledge.com>
2024-02-29 11:41:01 +0000

    Initial Commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Ferenc Visztra on 29/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
